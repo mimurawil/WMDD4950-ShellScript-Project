d436d12ca67752016640d6153e62163e9c6045c6
William T Mimura <williamtmimura@Williams-MacBook-Pro.local>
2017-09-30 14:22:59 -0700

    testing treatment of special characters

--- count.sh
+++ count.sh
@@ -2,18 +2,18 @@
 
 echo "SEARCH PROCEDURE"
 echo "Counting occurrences for the word $1"
-echo 
+echo
 
 TOTAL_COUNT=0
 
 for file in statistics/*.txt
 do
-  FIND_ARRAY=(`grep "^$1 => .*" $file`)
-  if [ "${FIND_ARRAY[2]}" != "" ];
-  then
-    echo "$file : ${FIND_ARRAY[@]}"
-  fi
-  TOTAL_COUNT=$((TOTAL_COUNT + FIND_ARRAY[2]))
+    FIND_ARRAY=(`grep "^$1 => .*" $file`)
+    if [ "${FIND_ARRAY[2]}" != "" ];
+    then
+        echo "$file : ${FIND_ARRAY[@]}"
+    fi
+    TOTAL_COUNT=$((TOTAL_COUNT + FIND_ARRAY[2]))
 done > "result.txt"
 #sed -i "TOTAL = $((TOTAL_COUNT))" "result.txt"
 echo "$TOTAL_COUNT occurrences found in all files"
@@ -22,8 +22,8 @@ echo "result.txt generated. Would you like to display the detail? (y/n)"
 read input
 if [ $input == "y" ] || [ $input == "Y" ];
 then
-  cat result.txt
+    cat result.txt
 else
-  echo "okay"
+    echo "okay"
 fi
 echo
--- crawler.sh
+++ crawler.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $1 == "" ] || [ $1 == "0" ];
 then
-  MAX_SITES=150
+    MAX_SITES=150
 else
-  MAX_SITES=$1
+    MAX_SITES=$1
 fi
 
 mkdir -p temp
@@ -12,7 +12,7 @@ mkdir -p sites
 
 echo "WMDD4950 - Security & Cloud & Server Admn"
 echo "William Takashi Mimura - id:100284327"
-echo 
+echo
 echo "Web crawler from https://en.wikipedia.org/wiki/Cloud_computing"
 echo
 echo "Max sites to crawl: $MAX_SITES"
@@ -31,56 +31,56 @@ ARRAY_LEN=${#MAIN_ARRAY[@]}
 MAIN_IND=0
 while [ $MAIN_IND -lt $ARRAY_LEN ] && [ $MAIN_IND -le $MAX_SITES ]
 do
-  # getting the name of the wiki page because it's stored as href="/wiki/page" so this command removes the href= and leaves only /wiki/page 
-  THIS_FILE=$(echo ${MAIN_ARRAY[$MAIN_IND]} | cut -d '"' -f 2)
-  # removing the /wiki/
-  THIS_FILE=${THIS_FILE:6}
-  
-  # download the page in queue
-  echo "Downloading page from https://en.wikipedia.org/wiki/${THIS_FILE}"
-  curl -s "https://en.wikipedia.org/wiki/${THIS_FILE}" > "sites/${THIS_FILE}.html"
-  
-  # verify if reached maximum sites, so we don't need to fetch and add more links
-  if [ $ARRAY_LEN -lt $MAX_SITES ];
-  then
-    echo "Fetching links..."
-    # search for href="/wiki/______" and store as array
-    TEMP_ARRAY=($(grep -o "\(href=\"\/wiki\/\w*\"\)" "sites/${THIS_FILE}.html"))
-    
-    echo "Adding links to the queue..."
-    # search in MAIN_ARRAY for each item of TEMP_ARRAY
-    ADDED=0
-    REPEATED=0
-    for i in "${TEMP_ARRAY[@]}" 
-    do
-      FOUND="n"
-      for j in "${MAIN_ARRAY[@]}"
-      do
-        if [ ${i,,} == ${j,,} ];
-        then
-          FOUND="y"
-          REPEATED=$((REPEATED + 1))
-          break
-        fi
-      done
-      
-      if [ "$FOUND" == "n" ];
-      then
-        MAIN_ARRAY+=($i)
-        ARRAY_LEN=${#MAIN_ARRAY[@]}
-        ADDED=$((ADDED + 1))
-  
-        if [ $ARRAY_LEN -ge $MAX_SITES ];
-        then
-          echo "Queue limit reached. Downloading the rest of the pages from queue..."
-          break
-        fi
-      fi
-    done
-    echo "$ADDED links added | $REPEATED links repeated (discarted)"
-  fi
-  
-  MAIN_IND=$((MAIN_IND + 1))
+    # getting the name of the wiki page because it's stored as href="/wiki/page" so this command removes the href= and leaves only /wiki/page
+    THIS_FILE=$(echo ${MAIN_ARRAY[$MAIN_IND]} | cut -d '"' -f 2)
+    # removing the /wiki/
+    THIS_FILE=${THIS_FILE:6}
+
+    # download the page in queue
+    echo "Downloading page from https://en.wikipedia.org/wiki/${THIS_FILE}"
+    curl -s "https://en.wikipedia.org/wiki/${THIS_FILE}" > "sites/${THIS_FILE}.html"
+
+    # verify if reached maximum sites, so we don't need to fetch and add more links
+    if [ $ARRAY_LEN -lt $MAX_SITES ];
+    then
+        echo "Fetching links..."
+        # search for href="/wiki/______" and store as array
+        TEMP_ARRAY=($(grep -o "\(href=\"\/wiki\/\w*\"\)" "sites/${THIS_FILE}.html"))
+
+        echo "Adding links to the queue..."
+        # search in MAIN_ARRAY for each item of TEMP_ARRAY
+        ADDED=0
+        REPEATED=0
+        for i in "${TEMP_ARRAY[@]}"
+        do
+            FOUND="n"
+            for j in "${MAIN_ARRAY[@]}"
+            do
+                if [ ${i,,} == ${j,,} ];
+                then
+                    FOUND="y"
+                    REPEATED=$((REPEATED + 1))
+                    break
+                fi
+            done
+
+            if [ "$FOUND" == "n" ];
+            then
+                MAIN_ARRAY+=($i)
+                ARRAY_LEN=${#MAIN_ARRAY[@]}
+                ADDED=$((ADDED + 1))
+
+                if [ $ARRAY_LEN -ge $MAX_SITES ];
+                then
+                    echo "Queue limit reached. Downloading the rest of the pages from queue..."
+                    break
+                fi
+            fi
+        done
+        echo "$ADDED links added | $REPEATED links repeated (discarted)"
+    fi
+
+    MAIN_IND=$((MAIN_IND + 1))
 done
 echo
 END_TIME_1=$(date +%s)
@@ -97,48 +97,49 @@ MAIN_IND=0
 while [ $MAIN_IND -lt $ARRAY_LEN ] && [ $MAIN_IND -le $MAX_SITES ]
 do
 
-  # getting the name of the wiki page because it's stored as href="/wiki/page" so this command removes the href= and leaves only /wiki/page 
-  THIS_FILE=$(echo ${MAIN_ARRAY[$MAIN_IND]} | cut -d '"' -f 2)
-  # removing the /wiki/
-  THIS_FILE=${THIS_FILE:6}
-  
-  # separating each word in a new line
-  echo "Treating file $THIS_FILE: getting words, treating and counting..."
-  # for word in `cat sites/"$THIS_FILE".html`; do echo $word; done > "temp/$THIS_FILE.txt"
-  for w in `lynx -dump sites/"$THIS_FILE".html`; do echo ${w,,}; done > "temp/$THIS_FILE.txt"
-  
-  # treating the file, some words are ending with ,.:;
-  sed -i "s/,$//g; s/\.$//g; s/:$//g; s/\;$//g" "temp/$THIS_FILE.txt"
-  # sorting the words for better counting algorithm
-  sort "temp/$THIS_FILE.txt" -o "temp/$THIS_FILE.sorted.txt"
-  lw=""
-  count=0
-  first="y"
-  # count algorithm, considering words are sorted and treated
-  for w in `cat "temp/$THIS_FILE.sorted.txt"`;
-  do
-    if [ $first == "y" ];
-    then
-      first="n"
-      lw=$w
-    fi
-    
-    if [ $w == $lw ];
-    then
-      count=$((count + 1))
-    else
-      echo "$lw => $count"
-      count=1
-    fi
-    
-    lw=$w
-  done > "temp/$THIS_FILE.counted.txt"
-  # adding the last word because of the algorithm
-  sed -i "\$a$lw => $count" "temp/$THIS_FILE.counted.txt"
-  # moving the final file to the right directory "statistics"
-  mv "temp/$THIS_FILE.counted.txt" "statistics/$THIS_FILE.txt"
-  
-  MAIN_IND=$((MAIN_IND + 1))
+    # getting the name of the wiki page because it's stored as href="/wiki/page" so this command removes the href= and leaves only /wiki/page
+    THIS_FILE=$(echo ${MAIN_ARRAY[$MAIN_IND]} | cut -d '"' -f 2)
+    # removing the /wiki/
+    THIS_FILE=${THIS_FILE:6}
+
+    # separating each word in a new line
+    echo "Treating file $THIS_FILE: getting words, treating and counting..."
+    # for word in `cat sites/"$THIS_FILE".html`; do echo $word; done > "temp/$THIS_FILE.txt"
+    for w in `lynx -dump sites/"$THIS_FILE".html`; do echo ${w,,}; done > "temp/$THIS_FILE.txt"
+
+    # treating the file, some words are ending with ,.:;
+    cat "temp/$THIS_FILE.txt" | tr -dc "[:alpha:]\.\n\r" > "temp/$THIS_FILE.treated.txt"
+    sed -i "s/,$//g; s/\.$//g; s/:$//g; s/\;$//g" "temp/$THIS_FILE.treated.txt"
+    # sorting the words for better counting algorithm
+    sort "temp/$THIS_FILE.treated.txt" -o "temp/$THIS_FILE.sorted.txt"
+    lw=""
+    count=0
+    first="y"
+    # count algorithm, considering words are sorted and treated
+    for w in `cat "temp/$THIS_FILE.sorted.txt"`;
+    do
+        if [ $first == "y" ];
+        then
+            first="n"
+            lw=$w
+        fi
+
+        if [ $w == $lw ];
+        then
+            count=$((count + 1))
+        else
+            echo "$lw => $count"
+            count=1
+        fi
+
+        lw=$w
+    done > "temp/$THIS_FILE.counted.txt"
+    # adding the last word because of the algorithm
+    sed -i "\$a$lw => $count" "temp/$THIS_FILE.counted.txt"
+    # moving the final file to the right directory "statistics"
+    mv "temp/$THIS_FILE.counted.txt" "statistics/$THIS_FILE.txt"
+
+    MAIN_IND=$((MAIN_IND + 1))
 done
 echo
 END_TIME_2=$(date +%s)
@@ -149,6 +150,6 @@ echo "Some temporary files were criated in the process, would you like to remove
 read input
 if [ $input == "y" ] || [ $input == "Y" ];
 then
-  rm -r temp/*
+    rm -r temp/*
 fi
 echo "done :)"
