c2be15deb88b4749b0a7417a6ac8f7ca9931e98c
William T Mimura <williamtmimura@Williams-MacBook-Pro.local>
2017-09-30 16:17:14 -0700

    bla

--- crawler.sh
+++ crawler.sh
@@ -105,20 +105,25 @@ do
     # separating each word in a new line
     echo "Treating file $THIS_FILE: getting words, treating and counting..."
     # for word in `cat sites/"$THIS_FILE".html`; do echo $word; done > "temp/$THIS_FILE.txt"
-    for w in `lynx -dump sites/"$THIS_FILE".html`;
-    do
-        if [ "$w" != "*" ];
-        then
-            echo "${w,,}";
-        fi 
-    done > "temp/$THIS_FILE.txt"
+    # for w in `lynx -dump sites/"$THIS_FILE".html`;
+    # do
+    #     if [ "$w" != "*" ];
+    #     then
+    #         echo "${w,,}";
+    #     fi
+    # done > "temp/$THIS_FILE.txt"
+    lynx -dump sites/"$THIS_FILE".html > temp/"$THIS_FILE".txt
 
     # treating the file, some words are ending with ,.:;
-    cat "temp/$THIS_FILE.txt" | tr -dc "[:alpha:]\-\/\_\.\n\r" > "temp/$THIS_FILE.treated.txt"
+    cat "temp/$THIS_FILE.txt" | tr -dc "[:alpha:] \-\/\_\.\n\r" | tr "[:upper:]" "[:lower:]" > "temp/$THIS_FILE.treated.txt"
     #sed -i "s/,$//g; s/\.$//g; s/:$//g; s/\;$//g" "temp/$THIS_FILE.treated.txt"
     sed -i "s/^file\/\/.*//g; s/^https\/\/.*//g; s/^http\/\/.*//g; s/^android-app\/\/.*//g; s/^-//g; s/^-//g; s/^-//g; s/^-//g; s/-$//g; s/,$//g; s/\.$//g; s/\.$//g; s/\.$//g; s/\/$//g; s/\.$//g; s/\.$//g; s/\.$//g; s/:$//g; s/\;$//g; /^$/d" "temp/$THIS_FILE.treated.txt"
+
+    for w in temp/"$THIS_FILE".treated.txt; do echo "$w"; done > temp/"$THIS_FILE".separated.txt
+
     # sorting the words for better counting algorithm
-    sort "temp/$THIS_FILE.treated.txt" -o "temp/$THIS_FILE.sorted.txt"
+    #sort "temp/$THIS_FILE.treated.txt" -o "temp/$THIS_FILE.sorted.txt"
+    sort "temp/$THIS_FILE.separated.txt" -o "temp/$THIS_FILE.sorted.txt"
     lw=""
     count=0
     first="y"
